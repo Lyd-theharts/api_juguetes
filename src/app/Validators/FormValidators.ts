372a4055299e5de64e7d7a4d5dedd00314fb428d
Lyd-theharts <lidia.lfg7@gmail.com>
2025-11-28 00:44:49 +0100

    primer commit con validaciones incluidas

---
+++ src/app/Validators/FormValidators.ts
@@ -0,0 +1,32 @@
+import {FormControl, ValidationErrors} from '@angular/forms';
+
+export class FormValidators {
+  static notOnlyWhiteSpace(control:FormControl): ValidationErrors | null {
+    if ((control.value != null) && (control.value.trim().length == 0)) { //lo del trim es que rellene solo con espacios.
+      return{notOnlyWhiteSpace: true};
+    }else{
+      return null;
+    }
+  }
+
+ /* static forbiddenName(text: string): ValidationErrors{
+    return (control: FormControl): ValidationErrors  | null => {
+      const regExp = new RegExp(text,'i'); //me pasa una expression regular, y con esa expresion regular testea si cualquier string que le pasamos cumple ese pattern que le hemos esstablecido en la expresion regular, la 'i' es que minusculas o mayusculas le da igual.
+      const forbidden = regExp.test(control.value);
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    }
+  }*/
+  static forbiddenName(text: string[]): ValidationErrors{ //funcion para hacer las palabras prohibidas en arrays.
+    return (control: FormControl): ValidationErrors  | null => {
+      let result = null;
+      text.forEach(p  => {
+        const regExp = new RegExp(p,'i'); //me pasa una expression regular, y con esa expresion regular testea si cualquier string que le pasamos cumple ese pattern que le hemos esstablecido en la expresion regular, la 'i' es que minusculas o mayusculas le da igual.
+        const forbidden = regExp.test(control.value);
+        if (forbidden) result =  {forbiddenName: {value: control.value}};
+      })
+      return result;
+
+    }
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {ListJuguetes} from './components/web/list-juguetes/list-juguetes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/list-juguetes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list-juguetes',
+    component: ListJuguetes
+  },
+  {
+    path: 'list-juguetes',
+    component: ListJuguetes
+  },
+
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  }
+];
